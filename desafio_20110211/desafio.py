52e0077cb00ec740aacc46b725d6d33d9ec7cc1f
Marcel Caraciolo <caraciol@gmail.com>
2011-02-12 15:21:15 -0300

    Minha resposta para o desafio.

--- desafio_20110211/desafio.py
+++ desafio_20110211/desafio.py
@@ -5,11 +5,14 @@ Semana: 11/02/2011
 
 Problema:
 
-    Dado uma lista de elementos, o objetivo eh converter esta lista em uma lista de sub-listas de elementos consecutivos duplicados. 
+    Dado uma lista de elementos, o objetivo eh converter esta lista em uma lista de sub-listas de elementos consecutivos duplicados.
+    Assim que um elemento subsequente for diferente do anterior, a sublista eh gerada e passa para o proximo elemento. Entao 
+    no exemplo abaixo dado 5 a's quando encontrar um b ele gera a sublista de 5 a's e comeca a gerar a sublista de b's ate encontrar um
+    elemento que nao seja b, e assim sucessivamente. 
     >>> x = ['a','a','a','a','b','c','c','a','a','d','e','e','e','e']
     >>> ret = pack(x)
     >>> ret
-    >>> [['a','a','a','a','a','a'],['b'],['c','c'],['d'],['e','e','e','e']]
+    >>> [['a','a','a','a'],['b'],['c','c'],['a','a'],['d'],['e','e','e','e']]
     >>> x = ['a', 'b', 'c']
     >>> ret = pack(x)
     >>> ret 
@@ -25,13 +28,40 @@ Problema:
 
 import unittest
 
+def pack(lista):
+    result =[]
+    sublista = []
+    while lista:
+        elemento = lista[0]
+        sublista+= [elemento] if elemento in sublista or not sublista else []
+        if elemento not in sublista:
+            result.append(sublista)
+            sublista = [elemento]
+        lista = lista[1:]
+    if sublista: result.append(sublista)
+    return result
 
+
+        
 class Desafio1(unittest.TestCase):
 
     def test_pack_duplicates(self):
         sampleList = ['a','a','a','a','b','c','c','a','a','d','e','e','e','e']
-        self.assertEqual([['a','a','a','a', 'a', 'a'],['b'],['c','c'],['d'],['e','e','e','e']],
+        self.assertEqual([['a','a','a','a'],['b'],['c','c'],['a','a'],['d'],['e','e','e','e']],
                     pack(sampleList))
+
+    def test_pack_empty(self):
+        sampleList = []
+        self.assertEqual([],
+                    pack(sampleList))
+
+
+    def test_pack_simple(self):
+        sampleList = ['a','b','c']
+        self.assertEqual([['a'],['b'], ['c']],
+                    pack(sampleList))
+
+
 
 
 if __name__ == '__main__':
